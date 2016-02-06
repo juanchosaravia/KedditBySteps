4c049c51e5295d292da2ff38dce4bb2c71c9e694
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-02-06 18:30:23 -0300

    updating part 2

--- app/src/main/java/com/droidcba/kedditbysteps/MainActivity.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.droidcba.kedditbysteps
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,28 +13,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction();
+        if (cleanStack) {
+            clearBackStack();
         }
+        ft.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit);
+        ft.replace(R.id.activity_base_content, f);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
 
-        return super.onOptionsItemSelected(item)
+    fun clearBackStack() {
+        val manager = supportFragmentManager;
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0);
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+
+    /**
+     * Finish activity when reaching the last fragment.
+     */
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager;
+        if (fragmentManager.backStackEntryCount > 1) {
+            fragmentManager.popBackStack();
+        } else {
+            finish();
+        }
     }
 }
