1afe0a3d343eb2353c97af4c2d1ac03def77a692
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-02-14 14:01:12 -0300

    part 3: android extensions

--- app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
@@ -9,19 +9,20 @@ import android.view.View
 import android.view.ViewGroup
 import com.droidcba.kedditbysteps.R
 import com.droidcba.kedditbysteps.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
-    private var newsList: RecyclerView? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //val view = inflater.inflate(R.layout.news_fragment, container, false)
-        val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true) // use this setting to improve performance
-        newsList?.layoutManager = LinearLayoutManager(context)
+        return container?.inflate(R.layout.news_fragment)
+    }
 
-        return view
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
     }
 }
 
