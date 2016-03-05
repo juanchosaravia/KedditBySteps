3c200af22879e3ec16aaa963ca38ec7e97659971
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-03-05 19:40:31 -0300

    part 4: creating RedditNewsItem data class

---
+++ app/src/main/java/com/droidcba/kedditbysteps/commons/Models.kt
@@ -0,0 +1,10 @@
+package com.droidcba.kedditbysteps.commons
+
+data class RedditNewsItem(
+        val author: String,
+        val title: String,
+        val numComments: Int,
+        val created: Long,
+        val thumbnail: String,
+        val url: String
+)
