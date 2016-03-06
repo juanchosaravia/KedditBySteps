205258e841d4cc2203cfbcc3e33165fa8a105dce
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-03-05 21:19:49 -0300

    part 4: addNews method for NewsAdapter with mock news

--- app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.droidcba.kedditbysteps.R
+import com.droidcba.kedditbysteps.commons.RedditNewsItem
 import com.droidcba.kedditbysteps.commons.extensions.inflate
 import com.droidcba.kedditbysteps.features.news.adapter.NewsAdapter
 import kotlinx.android.synthetic.main.news_fragment.*
@@ -23,8 +24,22 @@ class NewsFragment : Fragment() {
 
         news_list.setHasFixedSize(true)
         news_list.layoutManager = LinearLayoutManager(context)
-
         initAdapter()
+
+        if (savedInstanceState == null) {
+            val news = mutableListOf<RedditNewsItem>()
+            for (i in 1..10) {
+                news.add(RedditNewsItem(
+                        "author$i",
+                        "Title $i",
+                        i, // number of comments
+                        1457207701L - i * 200, // time
+                        "http://lorempixel.com/200/200/technics/$i", // image url
+                        "url"
+                ))
+            }
+            (news_list.adapter as NewsAdapter).addNews(news)
+        }
     }
 
     private fun initAdapter() {
--- app/src/main/java/com/droidcba/kedditbysteps/features/news/adapter/NewsAdapter.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/adapter/NewsAdapter.kt
@@ -3,6 +3,7 @@ package com.droidcba.kedditbysteps.features.news.adapter
 import android.support.v4.util.SparseArrayCompat
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import com.droidcba.kedditbysteps.commons.RedditNewsItem
 import com.droidcba.kedditbysteps.commons.adapter.AdapterConstants
 import com.droidcba.kedditbysteps.commons.adapter.ViewType
 import com.droidcba.kedditbysteps.commons.adapter.ViewTypeDelegateAdapter
@@ -39,4 +40,32 @@ class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         return this.items.get(position).getViewType()
     }
 
+    fun addNews(news: List<RedditNewsItem>) {
+        // first remove loading and notify
+        val initPosition = items.size - 1
+        items.removeAt(initPosition)
+        notifyItemRemoved(initPosition)
+
+        // insert news and the loading at the end of the list
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeChanged(initPosition, items.size + 1 /* plus loading item */)
+    }
+
+    fun clearAndAddNews(news: List<RedditNewsItem>) {
+        items.clear()
+        notifyItemRangeRemoved(0, getLastPosition())
+
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeInserted(0, items.size)
+    }
+
+    fun getNews(): List<RedditNewsItem> {
+        return items
+                .filter { it.getViewType() == AdapterConstants.NEWS }
+                .map { it as RedditNewsItem }
+    }
+
+    private fun getLastPosition() = if (items.lastIndex == -1) 0 else items.lastIndex
 }
