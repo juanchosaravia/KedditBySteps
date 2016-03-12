1623947fa709ba156d41d7a67f016871c8e9bd0d
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-03-12 16:12:31 -0300

    part 5: adding rxjava and news manager

--- app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsFragment.kt
@@ -7,15 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.droidcba.kedditbysteps.R
-import com.droidcba.kedditbysteps.commons.RedditNewsItem
 import com.droidcba.kedditbysteps.commons.extensions.inflate
 import com.droidcba.kedditbysteps.features.news.adapter.NewsAdapter
 import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
+    private val newsManager by lazy { NewsManager() }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //val view = inflater.inflate(R.layout.news_fragment, container, false)
         return container?.inflate(R.layout.news_fragment)
     }
 
@@ -27,19 +27,12 @@ class NewsFragment : Fragment() {
         initAdapter()
 
         if (savedInstanceState == null) {
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
-            }
-            (news_list.adapter as NewsAdapter).addNews(news)
+            requestNews()
         }
+    }
+
+    private fun requestNews() {
+        // (news_list.adapter as NewsAdapter).addNews(news)
     }
 
     private fun initAdapter() {
---
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsManager.kt
@@ -0,0 +1,31 @@
+package com.droidcba.kedditbysteps.features.news
+
+import com.droidcba.kedditbysteps.commons.RedditNewsItem
+import rx.Observable
+
+/**
+ * News Manager allows you to request more news from Reddit.
+ *
+ * @author juancho
+ */
+class NewsManager() {
+
+    fun getNews(): Observable<List<RedditNewsItem>> {
+        return Observable.create {
+            subscriber ->
+
+            val news = mutableListOf<RedditNewsItem>()
+            for (i in 1..10) {
+                news.add(RedditNewsItem(
+                        "author$i",
+                        "Title $i",
+                        i, // number of comments
+                        1457207701L - i * 200, // time
+                        "http://lorempixel.com/200/200/technics/$i", // image url
+                        "url"
+                ))
+            }
+            subscriber.onNext(news)
+        }
+    }
+}
