2f90d6dc1369b60ad8ea1382764045dcf730ef76
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-04-01 22:34:39 -0300

    part 6: consuming api from newsManager.

--- app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsManager.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/features/news/NewsManager.kt
@@ -1,5 +1,6 @@
 package com.droidcba.kedditbysteps.features.news
 
+import com.droidcba.kedditbysteps.api.RestAPI
 import com.droidcba.kedditbysteps.commons.RedditNewsItem
 import rx.Observable
 
@@ -8,25 +9,25 @@ import rx.Observable
  *
  * @author juancho
  */
-class NewsManager() {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
-            subscriber.onCompleted()
         }
     }
 }
