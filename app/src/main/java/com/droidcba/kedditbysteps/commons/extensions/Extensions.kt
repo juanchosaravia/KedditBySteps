d4dabc45ca967d0f673983ce4b382303deddbb6e
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-04-28 23:51:52 -0300

    Part 8: Extension Function for Creator

--- app/src/main/java/com/droidcba/kedditbysteps/commons/extensions/Extensions.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/commons/extensions/Extensions.kt
@@ -2,6 +2,8 @@
 
 package com.droidcba.kedditbysteps.commons.extensions
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -22,3 +24,11 @@ fun ImageView.loadImg(imageUrl: String) {
         Picasso.with(context).load(imageUrl).into(this)
     }
 }
+
+// Inline function to create Parcel Creator
+inline fun <reified T : Parcelable> createParcel(
+        crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
+        object : Parcelable.Creator<T> {
+            override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
+            override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
+        }
