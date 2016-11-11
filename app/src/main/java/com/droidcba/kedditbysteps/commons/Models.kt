56111c99f1d682fa7aee784a677775b1eb8d66a7
Juancho Saravia <juanchosaravia@gmail.com>
2016-11-11 12:40:45 -0300

    updating kotlin to 1.0.5 and fixing parcelable creator issue.

--- app/src/main/java/com/droidcba/kedditbysteps/commons/Models.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/commons/Models.kt
@@ -4,33 +4,27 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.droidcba.kedditbysteps.commons.adapter.AdapterConstants
 import com.droidcba.kedditbysteps.commons.adapter.ViewType
-import com.droidcba.kedditbysteps.commons.extensions.createParcel
 
 data class RedditNews(
         val after: String,
         val before: String,
         val news: List<RedditNewsItem>) : Parcelable {
-
     companion object {
-        @JvmField @Suppress("unused")
-        val CREATOR = createParcel { RedditNews(it) }
+        @JvmField val CREATOR: Parcelable.Creator<RedditNews> = object : Parcelable.Creator<RedditNews> {
+            override fun createFromParcel(source: Parcel): RedditNews = RedditNews(source)
+            override fun newArray(size: Int): Array<RedditNews?> = arrayOfNulls(size)
+        }
     }
 
-    protected constructor(parcelIn: Parcel) : this(
-            parcelIn.readString(),
-            parcelIn.readString(),
-            mutableListOf<RedditNewsItem>().apply {
-                parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
-            }
-    )
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(after)
-        dest.writeString(before)
-        dest.writeTypedList(news)
-    }
+    constructor(source: Parcel) : this(source.readString(), source.readString(), source.createTypedArrayList(RedditNewsItem.CREATOR))
 
     override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(after)
+        dest?.writeString(before)
+        dest?.writeTypedList(news)
+    }
 }
 
 data class RedditNewsItem(
@@ -42,30 +36,25 @@ data class RedditNewsItem(
         val url: String
 ) : ViewType, Parcelable {
 
+    override fun getViewType() = AdapterConstants.NEWS
+
     companion object {
-        @JvmField @Suppress("unused")
-        val CREATOR = createParcel { RedditNewsItem(it) }
+        @JvmField val CREATOR: Parcelable.Creator<RedditNewsItem> = object : Parcelable.Creator<RedditNewsItem> {
+            override fun createFromParcel(source: Parcel): RedditNewsItem = RedditNewsItem(source)
+            override fun newArray(size: Int): Array<RedditNewsItem?> = arrayOfNulls(size)
+        }
     }
 
-    protected constructor(parcelIn: Parcel) : this(
-            parcelIn.readString(),
-            parcelIn.readString(),
-            parcelIn.readInt(),
-            parcelIn.readLong(),
-            parcelIn.readString(),
-            parcelIn.readString()
-    )
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(author)
-        dest.writeString(title)
-        dest.writeInt(numComments)
-        dest.writeLong(created)
-        dest.writeString(thumbnail)
-        dest.writeString(url)
-    }
+    constructor(source: Parcel) : this(source.readString(), source.readString(), source.readInt(), source.readLong(), source.readString(), source.readString())
 
     override fun describeContents() = 0
 
-    override fun getViewType() = AdapterConstants.NEWS
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(author)
+        dest?.writeString(title)
+        dest?.writeInt(numComments)
+        dest?.writeLong(created)
+        dest?.writeString(thumbnail)
+        dest?.writeString(url)
+    }
 }
--- app/src/main/java/com/droidcba/kedditbysteps/commons/extensions/Extensions.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/commons/extensions/Extensions.kt
@@ -2,8 +2,6 @@
 
 package com.droidcba.kedditbysteps.commons.extensions
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -24,11 +22,3 @@ fun ImageView.loadImg(imageUrl: String) {
         Picasso.with(context).load(imageUrl).into(this)
     }
 }
-
-// Inline function to create Parcel Creator
-inline fun <reified T : Parcelable> createParcel(
-        crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
-        object : Parcelable.Creator<T> {
-            override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
-            override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
-        }
