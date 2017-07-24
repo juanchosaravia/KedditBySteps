7d13b14d1e7baefabb795bdc016e91c06cae3ea0
Fabio Hiroki <fhiroki@nic.br>
2017-07-24 15:09:11 -0300

    Remove unnecessary semicolons

--- app/src/main/java/com/droidcba/kedditbysteps/api/RedditApi.kt
+++ app/src/main/java/com/droidcba/kedditbysteps/api/RedditApi.kt
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 interface  RedditApi {
     @GET("/top.json")
     fun getTop(@Query("after") after: String,
-               @Query("limit") limit: String): Call<RedditNewsResponse>;
+               @Query("limit") limit: String): Call<RedditNewsResponse>
 }
